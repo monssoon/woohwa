cfebbca5923cda953757f99748f5ab8001fb2f9a
taewookimfardark <2012142087@yonsei.ac.kr>
2016-08-03 23:32:03 +0900

    add service and controller

---
+++ app.js
@@ -0,0 +1,20 @@
+var bucket_app = angular.module("bucket_app", ['ui.router']);
+
+
+bucket_app.config(function($stateProvider, $urlRouterProvider)
+{
+    $urlRouterProvider.otherwise("/main");
+
+    $stateProvider
+        .state('main',{
+            url: "/main",
+            templateUrl: "main_cover.html"
+        })
+        .state('current_bucket_list',{
+            url: "/currentbucketlist",
+            templateUrl: "main_current_bucket_list.html"
+        });
+        
+
+
+});
