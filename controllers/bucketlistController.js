cc04ca6f96af80c10a65c0580032651684e3cac5
taewookimfardark <2012142087@yonsei.ac.kr>
2016-08-08 18:36:58 +0900

    버킷 페이지 서버 불러오기

--- controllers/bucketlistController.js
+++ controllers/bucketlistController.js
@@ -2,15 +2,16 @@ bucket_app.controller("bucketlistController", ["$scope", "httpRequest", function
 
     $scope.taewoo = {"email": '', "profileurl": ''};
     $scope.jaewha = {"email": '', "profileurl": ''};
+
     httpRequest.send('GET','users')
         .then(
             function(res)
             {
                 console.log("success");
-                $scope.taewoo.email = res.data.data[0].email
-                $scope.taewoo.profileurl = res.data.data[0].photourl
-                $scope.jaewha.email = res.data.data[1].email
-                $scope.jaewha.profileurl = res.data.data[1].photourl
+                $scope.taewoo.email = res.data.data[0].email;
+                $scope.taewoo.profileurl = res.data.data[0].photourl;
+                $scope.jaewha.email = res.data.data[1].email;
+                $scope.jaewha.profileurl = res.data.data[1].photourl;
 
             }
             ,
@@ -21,4 +22,20 @@ bucket_app.controller("bucketlistController", ["$scope", "httpRequest", function
                 console.log(res);
             }
         );
+
+    httpRequest.send('GET','buckets/uncomplete')
+        .then(
+            function(res)
+            {
+                console.log(res);
+                $scope.bucketlist = res.data.data;
+                console.log($scope.bucketlist);
+            }
+            ,
+            function (res) {
+                alert("fail");
+                console.log(res);
+            }
+        );
+
 }]);
